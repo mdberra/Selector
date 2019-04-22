70713e8af3553a550f2f7e4c1eee208b8b859511
mdberra <marcelo.berra@gmail.com>
2019-04-22 13:19:14 -0300

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EstadoOptions, Estado } from './model/estadoOptions';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Selector';
+
+  estadoOptions: EstadoOptions;
+  optionsDeleg: any = [];   // idEstado, descripcion
+
+  delegacionForm: FormGroup;
+
+
+  //  estadoDescripOption: EstadoDescripOption;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.delegacionForm = this.formBuilder.group({
+      deleg: ['']
+    }, {
+      //   validator:
+    });
+
+    this.estadoOptions = new EstadoOptions();
+    this.estadoOptions.setEstados(Number(1), "Caso 1");
+    this.estadoOptions.setEstados(Number(2), "Caso 2");
+    this.estadoOptions.setEstados(Number(3), "Caso 3");
+    this.optionsDeleg =  this.estadoOptions.getEstados();
+    console.log(this.optionsDeleg);
+    console.log(this.estadoOptions.getEstadoId(2)[0].descripcion);
+    this.delegacionForm.controls['deleg'].setValue(this.estadoOptions.getEstadoId(2)[0].idEstado);
+  }
+
+  onSubmit() {
+    console.log(this.delegacionForm.controls['deleg'].value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
